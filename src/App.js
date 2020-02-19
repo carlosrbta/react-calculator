f6d142a1bcc9d72dcc8e41a6b01ce517292d20c5
Carlos <carlosrbta@gmail.com>
2020-02-18 23:43:05 -0300

    Layout base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,139 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+
+import Form from "react-bootstrap/Form";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Form className="calculator">
+        <Row>
+          <Col>
+            <Form.Control />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <Button variant="secondary" block>
+              (
+            </Button>
+          </Col>
+          <Col>
+            <Button variant="secondary" block>
+              )
+            </Button>
+          </Col>
+          <Col>
+            <Button variant="secondary" block>
+              %
+            </Button>
+          </Col>
+          <Col>
+            <Button variant="warning" block>
+              CE
+            </Button>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <Button variant="light" block>
+              7
+            </Button>
+          </Col>
+          <Col>
+            <Button variant="light" block>
+              8
+            </Button>
+          </Col>
+          <Col>
+            <Button variant="light block" block>
+              9
+            </Button>
+          </Col>
+          <Col>
+            <Button variant="secondary" block>
+              /
+            </Button>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <Button variant="light" block>
+              4
+            </Button>
+          </Col>
+          <Col>
+            <Button variant="light" block>
+              5
+            </Button>
+          </Col>
+          <Col>
+            <Button variant="light block" block>
+              6
+            </Button>
+          </Col>
+          <Col>
+            <Button variant="secondary" block>
+              *
+            </Button>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <Button variant="light" block>
+              1
+            </Button>
+          </Col>
+          <Col>
+            <Button variant="light" block>
+              2
+            </Button>
+          </Col>
+          <Col>
+            <Button variant="light block" block>
+              3
+            </Button>
+          </Col>
+          <Col>
+            <Button variant="secondary" block>
+              -
+            </Button>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <Button variant="light" block>
+              0
+            </Button>
+          </Col>
+          <Col>
+            <Button variant="light" block>
+              .
+            </Button>
+          </Col>
+          <Col>
+            <Button variant="primary" block>
+              =
+            </Button>
+          </Col>
+          <Col>
+            <Button variant="secondary" block>
+              +
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+    </Container>
   );
 }
 
